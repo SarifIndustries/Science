5a432c5ca2498f8021a120b0bcc2c6bda7693e9e
Dmitriy Severov <Dmitriy.Severov@bia-tech.ru>
2022-02-09 02:32:10 +0300

    Style corrections

--- montepi.py
+++ montepi.py
@@ -6,30 +6,34 @@ title="""
 #==========================================#
 """
 
+# Amount of points to drop.
+TOTAL_POINTS = 10000000
+
 from random import Random
 from math import sqrt
 
-def is_point_in_circle_segment(x: float, y: float) -> bool:
-	return x * x + y * y < 1
+def did_point_land_inside_circle_segment(x: float, y: float) -> bool:
+	return x * x + y * y <= 1
 
 #===================== MAIN =====================
 
 def main():
 	print(title)
 	r = Random()
-	points_in, points_out = 0, 0
-	for i in range(1000000):
-		x = r.random()
-		y = r.random()
-		if is_point_in_circle_segment(x, y):
-			points_in += 1
-		else:
-			points_out += 1
-	print(f"Points inside circle segment: \t{points_in}")
-	print(f"Points outside circle segment: \t{points_out}")
-	# Calculate Area difference
-	pi = sqrt(4 * points_in / points_out)
-	print(f"Pi value = {pi}") # TODO: check why this is incorrect.
+	total_points = TOTAL_POINTS
+	inner_points = 0
+	# Throwing points into area
+	print("Start dropping points...")
+	for i in range(total_points):
+		x, y = r.random(), r.random()
+		if did_point_land_inside_circle_segment(x, y):
+			inner_points += 1
+	# Total points
+	print(f"Points inside circle segment: \t{inner_points}")
+	print(f"Total points dropped:         \t{total_points}")
+	# Calculate Pi using area difference
+	pi = 4 * inner_points / total_points
+	print(f"Pi value = \t[ {pi} ]")
 
 #================================================
 
